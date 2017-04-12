349283bfd3248a4f9b2f2675f12ce7ece0ebfbc9
zohreh <eternalmary100@gmail.com>
2017-04-12 13:23:26 -0500

    Demo

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth import views as auth_views
-#from  instore.blog import views as blog_views
 
 urlpatterns = [
      url(r'^$', auth_views.login, name='home'),
