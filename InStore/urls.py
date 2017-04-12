c9a5cd4c693fa33cd46dfc8337712c8b9a1e6ab3
zohreh <eternalmary100@gmail.com>
2017-04-12 13:17:38 -0500

    Demo

--- InStore/urls.py
+++ InStore/urls.py
@@ -22,6 +22,10 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^login/$', auth_views.login, name='home'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^$', views.home, name='home'),
+    url(r'^$', auth_views.login, name='home'),
 
     # url(r'^accounts/', include('allauth.urls'), name='facebook'),
     #url(r'^settings/$', core_views.settings, name='settings'),
