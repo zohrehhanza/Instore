7ae4d91f2f97efa25a29197e1b550598b6d170f5
zohreh <eternalmary100@gmail.com>
2017-04-12 13:30:21 -0500

    Demo

--- InStore/urls.py
+++ InStore/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from  blog import views as blog_views
+#from  blog import views as blog_views
 
 
 urlpatterns = [
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^login/$', auth_views.login, name='home'),
     #url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^$', blog_views.home, name='home'),
+    #url(r'^$', blog_views.home, name='home'),
     url(r'^$', auth_views.login, name='home'),
 
 ]
