df98f19d920041b651eba6270701645bea24699f
zohreh <eternalmary100@gmail.com>
2017-04-13 00:23:09 -0500

    Demo

--- blog/views.py
+++ blog/views.py
@@ -53,7 +53,7 @@ def home(request):
         context = {doc['store'], doc['price'], doc['description']}
 
 
-    return render(request, 'blog/home.html', type(b))
+    return render(request, 'blog/home.html', b)
 
 #def search_result(request):
 
