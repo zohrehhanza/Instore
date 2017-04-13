a1a9c2db7a34b2ed64efed14b7129257c7602b54
zohreh <eternalmary100@gmail.com>
2017-04-13 05:20:24 -0500

    Demo

--- InStore/urls.py
+++ InStore/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     #url(r'^logout/$', auth_views.logout, name='logout'),
     #url(r'^$', blog_views.home, name='home'),
     url(r'^$', auth_views.login, name='home'),
-    #url(r'^accounts/profile',blog_views.home, name = 'home' ),
+    url(r'^accounts/profile',blog_views.home, name = 'home' ),
     url(r'^accounts/profile/?search_box=(?P<obj>[a-z])/$',blog_views.search_result, name = 'search_result')
-    url(r'^accounts/profile', blog_views.home, name='home'),
+    #url(r'^accounts/profile', blog_views.home, name='home'),
 
 ]
