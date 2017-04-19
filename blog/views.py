41b7b23304073a3132e6b77ef4d6a769c21c9df5
in-store <maryrasti91@gmail.com>
2017-04-19 15:46:08 -0400

    'demo'

--- blog/views.py
+++ blog/views.py
@@ -17,31 +17,6 @@ from PythonInsideHTML import PIH
 
 # Create your views here.
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-    
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
 
 #@login_required
 def home(request):
@@ -143,7 +118,3 @@ def home(request):
 
 
 
-    #webpage of detail of your hashtag
-#def hashdet(request, hashtag_name):
-  #  hash = get_object_or_404(Hashtag, name = hashtag_name)
-   # return render(request, 'main/hashdet.html', {'hash': hash})
