c8e2c24c6e66d0e7a3dc6b51e3be17fedd87d903
in-store <maryrasti91@gmail.com>
2017-04-19 15:47:40 -0400

    'demo'

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
          #     url(r'^$', views.post_list, name='post_list'),
     #url(r'^oauth/', include('social_django.urls', namespace='social')),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     #url(r'^accounts/', include('allauth.urls'), name='facebook'),
     url(r'^login/$', auth_views.login, name='home'),
     url(r'^logout/$', auth_views.logout, name='logout'),
