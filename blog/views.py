0dc5d0208082907967eeccb64b87d381123f4f65
in-store <maryrasti91@gmail.com>
2017-04-19 15:56:09 -0400

    'demo'

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,31 @@ from PythonInsideHTML import PIH
 
 # Create your views here.
 
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 #@login_required
 def home(request):
@@ -75,7 +100,7 @@ def home(request):
 
 
             len_doc2=len(Doc_2)
-            Doc_2=Doc_2.sort(key=lambda x: x[3])
+
 
             Dis_2_store=Distance(User_Lat,good_ltlng)
             sorted_Doc_2= Doc_2.sort()
@@ -118,3 +143,7 @@ def home(request):
 
 
 
+    #webpage of detail of your hashtag
+#def hashdet(request, hashtag_name):
+  #  hash = get_object_or_404(Hashtag, name = hashtag_name)
+   # return render(request, 'main/hashdet.html', {'hash': hash})
